d45a7df616a6b8c872a8c8f9326496c16361f3c2
awhite001 <ant.c.white@gmail.com>
2024-05-29 22:35:28 -0800

    Upload Marzipano 360 Example Site
    Created the 360 tour in the Marzipano Tool (https://www.marzipano.net/tool/). Uploading to host on Eduverse WP site.

---
+++ yankee-stadium-360/data.js
@@ -0,0 +1,104 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-yankee-stadium",
+      "name": "Yankee Stadium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.39128046561729235,
+        "pitch": 0.33416202528887595,
+        "fov": 1.4462663191090503
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5113255908053347,
+          "pitch": 1.343104408182608,
+          "rotation": 6.283185307179586,
+          "target": "0-yankee-stadium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03373883670037081,
+          "pitch": 0.5074914455053907,
+          "title": "Catcher",
+          "text": "Catches pitches from the pitcher and guards home plate."
+        },
+        {
+          "yaw": -0.13749228829017568,
+          "pitch": 0.3456225464406426,
+          "title": "Pitcher",
+          "text": "Throws the ball to the batter from the pitcher's mound."
+        },
+        {
+          "yaw": 0.08131179949581657,
+          "pitch": 0.34006150196431406,
+          "title": "<strong>First Baseman</strong>",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Covers first base, fields ground balls, and catches throws to get runners out.</span>"
+        },
+        {
+          "yaw": -0.3174357217716448,
+          "pitch": 0.27740709599666147,
+          "title": "Second Baseman",
+          "text": "Covers second base, fields ground balls, and turns double plays."
+        },
+        {
+          "yaw": -0.47239531651169386,
+          "pitch": 0.4082476120284113,
+          "title": "Third Baseman",
+          "text": "Covers third base, fields ground balls, and stops bunts."
+        },
+        {
+          "yaw": -0.45921240786530504,
+          "pitch": 0.31110086102106393,
+          "title": "Shortstop",
+          "text": "Fields ground balls and assists with plays at second and third base."
+        },
+        {
+          "yaw": -0.01929728198267,
+          "pitch": 0.21593893056471458,
+          "title": "Left Fielder",
+          "text": "Covers the left outfield and catches fly balls."
+        },
+        {
+          "yaw": -0.4041377813652094,
+          "pitch": 0.1690888848102201,
+          "title": "Center Fielder",
+          "text": "Covers the center outfield, catches fly balls, and backs up left and right fielders."
+        },
+        {
+          "yaw": -0.7510961913815333,
+          "pitch": 0.21840257534933372,
+          "title": "Right Fielder",
+          "text": "Covers the right outfield and catches fly balls."
+        }
+      ]
+    }
+  ],
+  "name": "Yankee Stadium",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
